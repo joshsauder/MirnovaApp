80adf97d9ea69d2d9eb15e2b7b8def9446c243d0
Josh Sauder <joshsauder@gmail.com>
2020-05-13 22:02:49 -0400

    Initial Commit

---
+++ Mirnova/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mirnova
+//
+//  Created by Josh Sauder on 5/13/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
