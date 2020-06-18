0d6a5c68646705af804e738aa2af1a908e4e6007
Josh Sauder <joshsauder@gmail.com>
2020-06-18 16:10:24 -0400

    added request for images

--- Mirnova/Views/CourseMaterial/CourseMaterialViewModel.swift
+++ Mirnova/Views/CourseMaterial/CourseMaterialViewModel.swift
@@ -14,14 +14,17 @@ class CourseMaterialViewModel: ObservableObject {
 
     var name: String
     var courseMaterial: [CourseMaterial] = []
+    var images: [UIImage] = []
     
     init(name: String) {
         self.name = name
-        fetchData()
+        fetchData(){
+            self.fetchImages(){ self.images = $0 }
+        }
     }
     
     
-    private func fetchData(){
+    private func fetchData(completion: @escaping () -> ()){
         Network.shared.apollo.fetch(query: CourseQuery(name: name)){ result in
             
             switch result {
@@ -30,10 +33,29 @@ class CourseMaterialViewModel: ObservableObject {
             case .success(let result):
                 let data = result.data!
                 self.courseMaterial = data.course?.questions.map({ self.buildCourseMaterial(question: $0)}) ?? []
+                completion()
             }
             
         }
         
+    }
+    
+    private func fetchImages(completion: @escaping ([UIImage]) -> ()){
+        let req = RestRequests()
+        var images: [UIImage] = []
+        let group = DispatchGroup()
+    
+        for (i, course) in courseMaterial.enumerated() {
+            group.enter()
+            req.getImage(image: course.image){
+                images.insert($0 ?? UIImage(), at: i)
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .main){
+            completion(images)
+        }
     }
     
     private func buildCourseMaterial(question: CourseQuery.Data.Course.Question) -> CourseMaterial {
