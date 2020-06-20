33aeb74fa762b8d2e1ac02734b1e602463914c30
Josh Sauder <joshsauder@gmail.com>
2020-06-20 12:03:11 -0400

    setup test view

--- Mirnova/Views/Test/TestView.swift
+++ Mirnova/Views/Test/TestView.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+extension TestViewController {
+    
+    func setupView() {
+        PromptLabel.text = "Select One"
+        
+        setUpButtons(button: AButton)
+        setUpButtons(button: BButton)
+        setUpButtons(button: CButton)
+        setUpButtons(button: DButton)
+    }
+    
+    private func setUpButtons(button: UIButton){
+        button.layer.cornerRadius = 0.5 * button.bounds.width
+        button.clipsToBounds = true
+        button.backgroundColor = .green
+    }
+}
