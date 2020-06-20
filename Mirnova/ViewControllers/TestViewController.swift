b24f2d257405b3c8056305b7435e299abdb55654
Josh Sauder <joshsauder@gmail.com>
2020-06-20 11:58:56 -0400

    setup options

--- Mirnova/ViewControllers/TestViewController.swift
+++ Mirnova/ViewControllers/TestViewController.swift
@@ -25,21 +25,75 @@ class TestViewController: UIViewController {
     var totalCorrect: Int = 0
     var totalAttempted: Int = 0
     
+    var correctIdx: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        SignImage.image = courseMaterial[0].image
+        
+        //randomize questions
+        courseMaterial.shuffle()
+        setViewItems()
     }
     
-    
-    func nextQuestion(){
-        totalAttempted += 1
-        
+    func setViewItems(){
         let currentItem = courseMaterial[totalAttempted]
         
-        
-        
+        //set up options and set images
+        setOptions(currentItem: currentItem)
         SignImage.image = currentItem.image
     }
+    
+    func setOptions(currentItem: CourseMaterial){
+        let randomNums = generateRandomNumbers(numQuestions: courseMaterial.count)
+        let firstIncorrect = courseMaterial[randomNums[0]]
+        let secondIncorrect = courseMaterial[randomNums[1]]
+        let thirdIncorrect = courseMaterial[randomNums[2]]
+        
+        switch correctIdx{
+        case 0:
+            //A is correct
+            AButton.setTitle(currentItem.answer, for: .normal)
+            
+            BButton.setTitle(firstIncorrect.answer, for: .normal)
+            CButton.setTitle(secondIncorrect.answer, for: .normal)
+            DButton.setTitle(thirdIncorrect.answer, for: .normal)
+        case 1:
+            //B is correct
+            BButton.setTitle(currentItem.answer, for: .normal)
+            
+            AButton.setTitle(firstIncorrect.answer, for: .normal)
+            CButton.setTitle(secondIncorrect.answer, for: .normal)
+            DButton.setTitle(thirdIncorrect.answer, for: .normal)
+        case 2:
+            //C is correct
+            CButton.setTitle(currentItem.answer, for: .normal)
+            
+            AButton.setTitle(firstIncorrect.answer, for: .normal)
+            BButton.setTitle(secondIncorrect.answer, for: .normal)
+            DButton.setTitle(thirdIncorrect.answer, for: .normal)
+            
+        default:
+            //D is correct
+            DButton.setTitle(currentItem.answer, for: .normal)
+            
+            AButton.setTitle(firstIncorrect.answer, for: .normal)
+            BButton.setTitle(secondIncorrect.answer, for: .normal)
+            CButton.setTitle(thirdIncorrect.answer, for: .normal)
+        }
+    }
+    
+    func generateRandomNumbers(numQuestions: Int) -> [Int] {
+        var randomSet = Set<Int>()
+        correctIdx = Int.random(in: 0 ... 2)
+        
+        while randomSet.count < 3 {
+            let randomNum = Int.random(in: 0 ..< numQuestions)
+            randomSet.insert(randomNum)
+        }
+        
+        return Array(randomSet)
+    }
+    
     
 }
 
---
+++ Mirnova/Views/Test/TestView.swift
@@ -0,0 +1,9 @@
+//
+//  TestView.swift
+//  Mirnova
+//
+//  Created by Josh Sauder on 6/20/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
