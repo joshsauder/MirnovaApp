a03ebe780f3394c8c5811dcf75fb92fabeb2faf6
Josh Sauder <joshsauder@gmail.com>
2020-07-06 22:36:08 -0400

    init results

---
+++ Mirnova/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ResultsViewController.swift
+//  Mirnova
+//
+//  Created by Josh Sauder on 7/6/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import UIKit
