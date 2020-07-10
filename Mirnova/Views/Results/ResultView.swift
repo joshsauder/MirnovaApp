87abc6eedd059f56a83e664de335d6913179cc56
Josh Sauder <joshsauder@gmail.com>
2020-07-09 21:55:16 -0400

    comments

--- Mirnova/Views/Results/ResultView.swift
+++ Mirnova/Views/Results/ResultView.swift
@@ -11,11 +11,29 @@ import UIKit
 
 extension ResultViewController {
     
+    /**
+    Sets up Result View
+     - parameters:
+        - correct: Number of correct questions
+        - incorrect: Number of incorrect questions
+        - attempt: Number of attemps
+        - average: Average correct for course
+        - passed: Booleen determining if user passed
+     */
     func setUpView(correct: Int, incorrect: Int, attempts: Int, average: Int, passed: Bool){
         setLabels(correct: correct, incorrect: incorrect, attempts: attempts, average: average, passed: passed)
         setImage(passed: passed)
     }
     
+    /**
+    Sets up Labels
+     - parameters:
+        - correct: Number of correct questions
+        - incorrect: Number of incorrect questions
+        - attempt: Number of attemps
+        - average: Average correct for course
+        - passed: Booleen determining if user passed
+     */
     private func setLabels(correct: Int, incorrect: Int, attempts: Int, average: Int, passed: Bool){
         self.FinalScoreLabel.text = "Final Score \n\(correct)/\(correct+incorrect)"
         
@@ -27,6 +45,11 @@ extension ResultViewController {
         self.PassLabel.text = passed ? "You Passed!" : "Try Again"
     }
     
+    /**
+    Sets up Result Image
+     - parameters:
+        - passed: Booleen determining if user passed
+     */
     private func setImage(passed: Bool){
         let image =  passed ? "checkmark.cirle" : "xmark.circle"
         let color = passed ? UIColor.green : UIColor.red
