e1b9c7eb3423dd197493fe6a6352e2924d607392
Josh Sauder <joshsauder@gmail.com>
2020-08-09 22:14:24 -0700

    insets fixed

--- Mirnova/ViewControllers/ResultsViewController.swift
+++ Mirnova/ViewControllers/ResultsViewController.swift
@@ -43,9 +43,6 @@ class ResultViewController: UIViewController, UITableViewDataSource, UITableView
         
         QuestionsTableView.isScrollEnabled = false
         ScrollView.contentInsetAdjustmentBehavior = .never
-        ScrollView.contentInset.top += 44
-        ScrollView.contentInset.bottom -= 44
-        
         
         self.setUpView(correct: totalCorrect, incorrect: userAnswers.count, attempts: attempts, average: average, passed: passed)
     }
@@ -78,7 +75,7 @@ extension ResultViewController {
             let prevSize = tableViewHeight.constant
             tableViewHeight.constant = cell.frame.height * CGFloat(userAnswers.count)
             ScrollView.contentSize.height = ScrollView.contentSize.height + (tableViewHeight.constant - prevSize)
-            InfoView.frame.size.height = InfoView.frame.size.height + (tableViewHeight.constant - prevSize) + 30
+            InfoView.frame.size.height = InfoView.frame.size.height + (tableViewHeight.constant - prevSize)
        }
         
         return cell
