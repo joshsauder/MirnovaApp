a69ecf3c63bc02d3b437c45150e64405847d1f48
Josh Sauder <joshsauder@gmail.com>
2020-09-04 22:32:04 -0700

    completed results circle

--- Mirnova/Views/Results/ResultsCircleView.swift
+++ Mirnova/Views/Results/ResultsCircleView.swift
@@ -9,16 +9,42 @@
 import SwiftUI
 
 struct ResultsCircleView: View {
+    var correct: Int
+    var total: Int
+    
+    var cicleBorderWidth = CGFloat(10)
+    
     var body: some View {
-        Circle()
-            .stroke(Color.green, lineWidth: 8)
-            .padding(8)
+        ZStack {
+            Circle()
+                .stroke(Color.green, lineWidth: cicleBorderWidth)
+                .opacity(0.3)
+            Circle()
+                .trim(from: 0, to: finalScore())
+                .stroke(style: StrokeStyle(lineWidth: cicleBorderWidth, lineCap: .round, lineJoin: .round))
+                .foregroundColor(.green)
+                .rotationEffect(.degrees(-90))
+                .animation(.easeInOut(duration: 0.2))
+        }
+        .padding(cicleBorderWidth)
         
+    }
+    
+    /**
+     Calculates the users final score
+     
+     - returns: The users final score
+     */
+    func finalScore() -> CGFloat {
+        return CGFloat(correct) / CGFloat(total)
     }
 }
 
 struct ResultsCircleView_Previews: PreviewProvider {
+    static var correct = 2
+    static var total = 4
+    
     static var previews: some View {
-        ResultsCircleView()
+        ResultsCircleView(correct: correct, total: total)
     }
 }
