94c5495d5e68cdcd1227a2521190a227a0120b57
Josh Sauder <joshsauder@gmail.com>
2020-10-07 21:58:16 -0700

    project restructure

---
+++ Mirnova/Views/SharedViews/NavContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  NavContainerView.swift
+//  Mirnova
+//
+//  Created by Josh Sauder on 10/7/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavContainerView()
+    }
+}
