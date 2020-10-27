e93aff79fa482ef167704bcef44a4f2389e6975b
Josh Sauder <joshsauder@gmail.com>
2020-10-26 20:06:07 -0700

    added image cache struct

---
+++ Mirnova/Services/ImageCache.swift
@@ -0,0 +1,72 @@
+//
+//  ImageCache.swift
+//  Mirnova
+//
+//  Created by Josh Sauder on 10/26/20.
+//  Copyright © 2020 Josh Sauder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ImageCache {
+    
+    /**
+    Deletes an image in the cache
+     
+     - parameters:
+        - name: The name of the image
+     */
+    static func delete(imageNamed name: String) {
+        guard let imagePath = ImageCache.path(for: name) else {
+            return
+        }
+        
+        try? FileManager.default.removeItem(at: imagePath)
+    }
+    
+    /**
+    Retrieves an image in the cache
+     
+     - parameters:
+        - name: The name of the image
+     */
+    static func retrieve(imageNamed name: String) -> UIImage? {
+        guard let imagePath = ImageCache.path(for: name) else {
+            return nil
+        }
+        
+        return UIImage(contentsOfFile: imagePath.path)
+    }
+    
+    /**
+    Stores an image in the cache
+     
+     - parameters:
+        - image: The UIImage
+        - name: The name of the image
+     */
+    static func store(image: UIImage, name: String) throws {
+        
+        guard let imageData = image.pngData() else {
+            throw NSError(domain: "com.thecodedself.imagestore", code: 0, userInfo: [NSLocalizedDescriptionKey: "The image could not be created"])
+        }
+        
+        guard let imagePath = ImageCache.path(for: name) else {
+            throw NSError(domain: "com.thecodedself.imagestore", code: 0, userInfo: [NSLocalizedDescriptionKey: "The image path could not be retrieved"])
+        }
+        
+        try imageData.write(to: imagePath)
+    }
+    
+    /**
+     Retrieves the path of the image
+     - parameters:
+        - for: The image name
+        - fileExtension: The extension of the file. Defaults to png
+     */
+    private static func path(for imageName: String, fileExtension: String = "png") -> URL? {
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        return directory?.appendingPathComponent("\(imageName).\(fileExtension)")
+    }
+}
